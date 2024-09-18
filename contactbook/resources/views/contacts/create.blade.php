df3927adade6e7715c611bba25d06069d6397740
unknown <gabrielvitortec@gmail.com>
2024-09-18 19:55:47 -0300

    view de criação

---
+++ contactbook/resources/views/contacts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+    <div class="container mt-5">
+        <h2>Adicionar Contato</h2>
+        <form action="{{ route('contacts.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Nome</label>
+                <input type="text" name="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone</label>
+                <input type="text" name="phone" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-success">Salvar</button>
+        </form>
+    </div>
+@endsection
