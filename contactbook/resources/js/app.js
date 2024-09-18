255300f7589a5f8bd217f6e5dd66343257e23b8a
unknown <gabrielvitortec@gmail.com>
2024-09-18 19:59:55 -0300

    config vue

--- contactbook/resources/js/app.js
+++ contactbook/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import ContactList from './components/ContactList.vue';
+
+const app = createApp({});
+app.component('contact-list', ContactList);
+app.mount('#app');
